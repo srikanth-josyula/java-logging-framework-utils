0ae18766f3731ee56d6752c9a139a8f132db24d5
Srikanth Josyula <srikanthjosyula@Srikanths-Air.hitronhub.home>
2024-02-17 12:45:59 -0400

    initial commit

---
+++ log4j2/src/main/java/com/sample/Log4j2Client.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2Client {
+
+	private static final Logger logger = LogManager.getLogger(Log4j2Client.class);
+
+	public static void main(String[] args) {
+		try {
+			logger.trace("Designates finer-grained informational events than the DEBUG.");
+			logger.debug("Designates fine-grained informational events that are most useful to debug an application.");
+
+			logger.info(
+					"Designates informational messages that highlight the progress of the application at coarse-grained level.");
+			logger.warn("Designates potentially harmful situations.");
+			throw new Exception();
+		} catch (Exception e) {
+			logger.error("Designates error events that might still allow the application to continue running.");
+			logger.fatal("Designates very severe error events that will presumably lead the application to abort.");
+		}
+	}
+}
